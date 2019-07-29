73e8a6e310bb6cc9ccdae9dffd96b9acf9a057eb
Bruce-zxy <bruce_zxy@163.com>
2019-07-29 14:22:38 +0800

    [feat]:暂时完成了我的中心

--- src/pages/mine.js
+++ src/pages/mine.js
@@ -4,4 +4,43 @@ import { TabBar } from 'antd-mobile';
 
 import "../style/mine.scss";
 
-export default () => <Link to="/app/lvyoto/project">"MINE PAGE"</Link>;
+export default (props) => {
+    
+
+
+    return (
+        <div className="hdz-lvyoto-mine">
+            <div className="mine-card">
+                <img src='http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image' alt='placeholder+image' />
+                <div className="mine-cart-info">
+                    <p>
+                        <span>小通通</span>
+                        <span><i className="iconfont iconkuozhanVIP"></i>0</span>
+                    </p>
+                    <p>律师事务所</p>
+                </div>
+            </div>
+
+            <Link to="javascript:;" className="upgrade-vip">升级VIP</Link>
+
+            <div className="mine-function">
+                <Link to="javascript:;" className="mine-function-item">
+                    <i className="iconfont iconjinrong"></i>
+                    <span>金融服务</span>
+                    <i className="iconfont iconyoubian"></i>
+                </Link>
+                <Link to="javascript:;" className="mine-function-item">
+                    <i className="iconfont iconfuwu-active"></i>
+                    <span>我的服务</span>
+                    <i className="iconfont iconyoubian"></i>
+                </Link>
+                <Link to="javascript:;" className="mine-function-item">
+                    <i className="iconfont iconmingpian2"></i>
+                    <span>名片管理</span>
+                    <i className="iconfont iconyoubian"></i>
+                </Link>
+            </div>
+            
+        </div>
+    )
+};
