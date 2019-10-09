a24ae162e04f549c89baa8627610abedd78cdcf5
Hirogue <314367747@qq.com>
2019-10-09 11:17:41 +0800

    fix:修复微信分享图片无法显示的问题

--- src/utils/global.js
+++ src/utils/global.js
@@ -261,7 +261,7 @@ export const toSetWeChatShareConfig = async (title, desc, img) => {
       timestamp, // 必填，生成签名的时间戳
       nonceStr, // 必填，生成签名的随机串
       signature, // 必填，签名，见附录1
-      jsApiList: ["checkJsApi", "updateAppMessageShareData", "updateTimelineShareData"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      jsApiList: ["checkJsApi", "updateAppMessageShareData", "updateTimelineShareData","onMenuShareAppMessage","onMenuShareTimeline"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
     window.wx.ready(function () {
       window.wx.updateAppMessageShareData({
