7f097ad17c0eb7c352cae8a4a19f1aebfaa03e08
Hirogue <314367747@qq.com>
2019-10-10 10:27:56 +0800

    fix:修改分享默认图片地址

--- src/utils/global.js
+++ src/utils/global.js
@@ -267,14 +267,14 @@ export const toSetWeChatShareConfig = async (title, desc, img) => {
       window.wx.updateAppMessageShareData({
         title: title || '旅游项目通', // 分享标题
         link: window.location.href, // 分享链接
-        imgUrl: img || 'https://www.lvyoto.com/static/logo.png', // 分享图标
+        imgUrl: img || 'https://www.lvyoto.com/static/img/share-logo.png', // 分享图标
         desc: desc || document.querySelector('meta[name="description"]').content, // 分享描述
       });
       //朋友
       window.wx.updateTimelineShareData({
         title: title || '旅游项目通', // 分享标题
         link: window.location.href, // 分享链接
-        imgUrl: img || 'https://www.lvyoto.com/static/logo.png', // 分享图标
+        imgUrl: img || 'https://www.lvyoto.com/static/img/share-logo.png', // 分享图标
       });
     });
   }
