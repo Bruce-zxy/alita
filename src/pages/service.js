3e237210f612a8777c9b3d43ff94df9cced944c8
Hirogue <314367747@qq.com>
2019-11-05 20:59:08 +0800

    fix:fix bug

--- src/pages/service.js
+++ src/pages/service.js
@@ -72,9 +72,8 @@ export default withApollo((props) => {
             defaultVariables.filter = [];
             thisState.category ? defaultVariables.filter.push({ field: "category.title", operator: CondOperator.EQUALS, value: thisState.category }) : '';
             thisState.area.length ? defaultVariables.filter.push({ field: "area.title", operator: CondOperator.EQUALS, value: thisState.area.pop() }) : '';
+            defaultVariables.filter.push({ field: "status", operator: CondOperator.IN, value: "checked,finished,waiting,following" });
         }
-
-        defaultVariables.filter.push({ field: "status", operator: CondOperator.IN, value: "checked,finished,waiting,following" }),
 
         defaultVariables.sort = [{
             field: 'create_at',
